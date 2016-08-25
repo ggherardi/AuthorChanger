261ed083c687078ece913ecfc0dcc5b230f4410d
Gianmattia <giamalfred@yahoo.it>
2016-08-25 20:30:30 +0200

    Start

---
+++ AuthorChanger/AuthorChanger/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace AuthorChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dialogChoosePath = new System.Windows.Forms.OpenFileDialog();
+            this.btnChoosePath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dialogChoosePath
+            // 
+            this.dialogChoosePath.FileName = "openFileDialog1";
+            // 
+            // btnChoosePath
+            // 
+            this.btnChoosePath.Location = new System.Drawing.Point(191, 38);
+            this.btnChoosePath.Name = "btnChoosePath";
+            this.btnChoosePath.Size = new System.Drawing.Size(75, 23);
+            this.btnChoosePath.TabIndex = 0;
+            this.btnChoosePath.Text = "button1";
+            this.btnChoosePath.UseVisualStyleBackColor = true;
+            this.btnChoosePath.Click += new System.EventHandler(this.btnChoosePath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 454);
+            this.Controls.Add(this.btnChoosePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog dialogChoosePath;
+        private System.Windows.Forms.Button btnChoosePath;
+    }
+}
+
---
+++ AuthorChanger/AuthorChanger/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuthorChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoosePath_Click(object sender, EventArgs e)
+        {
+            dialogChoosePath.ShowDialog();
+        }
+    }
+}
